245faf75ebbe2837700fad495a4d72a8d3fe3aee
bikza <63042594+bikza@users.noreply.github.com>
2021-11-03 19:40:10 +0300

    Add files via upload

---
+++ zadanie1.py
@@ -0,0 +1,10 @@
+a = input("A=")
+b = input("B=")
+c = input("C=")
+d = input("D=")
+
+d=c
+c=b
+b=a
+
+print('A=',a,'B=',b,'C=',c, 'D=',d)
---
+++ zadanie3.py
@@ -0,0 +1,8 @@
+a = input("a=")
+b = input("b=")
+c = input("c=")
+
+S=a+b
+S=S+c
+
+print('S=', S)
